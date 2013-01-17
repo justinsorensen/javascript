fcfe5e2249e102ea7eee8c2e9545397dc5eea5bc
Justin Sorensen <justinsorensen83@gmail.com>
2013-01-17 01:14:06 -0800

    Javascript inheritance experiments
    attempt at prototype chain and output loop

---
+++ classInheritance1.js
@@ -0,0 +1,31 @@
+// Make an animal class with attributes: name, numLegs and color.
+// exercise inspired by codecademy
+
+function Animal(name, numLegs, color) {
+    this.name = name;
+    this.numLegs = numLegs;
+    this.color = "brown";
+}
+Animal.prototype.sayName = function() {
+								document.write("Hi, I'm "+this.name+"! I have "+this.numLegs+" legs and I'm "+this.color+"!"); 
+};
+
+//Define dog class here
+function Dog(name) {
+	this.name = "blue";			//when changed to static, instance class name takes precedence.
+	this.numLegs = 4;
+						//no color attribute here to demonstrate inheritance hopefully?
+}
+
+function Teacup(name) {
+	this.name = name;
+}
+
+// prototype chain set here
+Dog.prototype = new Animal();
+Teacup.prototype = new Dog();
+
+var dog = new Teacup("tinkerbell");
+dog.sayName();
+
+
---
+++ classInheritance2.js
@@ -0,0 +1,36 @@
+// Make an animal class with attributes: name, numLegs and color.
+// exercise inspired by codecademy
+// attempt at looping output for large number of attributes.
+
+function Animal(name, numLegs, color) {
+    this.name = name;
+    this.numLegs = numLegs;
+    this.color = "brown";
+}
+Animal.prototype.sayName = function() {
+							for(var property in this)
+							document.write(this[property]+" ");  //outputs properties but also outputs the function, must fix
+};
+
+//Define dog class here
+function Dog(name) {
+	this.name = "blue";			//when changed to static, instance class name takes precedence.
+	this.numLegs = 4;
+						//no color attribute here to demonstrate inheritance hopefully?
+}
+
+function Teacup(name) {
+	this.name = name;
+}
+
+// prototype chain set here
+Dog.prototype = new Animal();
+Teacup.prototype = new Dog();
+
+var dog = new Teacup("tinkerbell");
+dog.sayName();
+
+
+
+//Stored for replacement in sayName
+//document.write("Hi, I'm "+this.name+"! I have "+this.numLegs+" legs and I'm "+this.color+"!"); 
